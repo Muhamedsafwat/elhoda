b085a76b5476ea8a4a374bb5c1bb657cf7d48be6
Muhamedsafwat <musafwat666@gmail.com>
2024-11-24 15:33:09 +0200

    responsive navbar

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 // import images
 import logo from "public/vectors/elHodaLogo 4.svg";
 //import icons
-import { Home, Info, Phone } from "lucide-react";
+import { Home, Info, Phone, Menu, XIcon } from "lucide-react";
 
 const navItems = [
   {
@@ -25,8 +26,10 @@ const navItems = [
 ];
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header className="gradient-l py-3 top-0 w-screen box-border overflow-hidden z-20 shadow-xl fixed opacity-90 hover:opacity-100 duration-200">
+    <header className="gradient-l py-3 top-0 w-screen box-border z-20 shadow-xl fixed opacity-90 hover:opacity-100 duration-200">
       <div className="container flex flex-row-reverse justify-between items-center">
         {/*link & logo*/}
         <Link href="/" className="flex items-center gap-1">
@@ -35,7 +38,8 @@ const NavBar = () => {
             الهدي جروب
           </h1>
         </Link>
-        <nav>
+        {/*desktop navbar*/}
+        <nav className="hidden md:block">
           <ul className="flex items-center gap-16">
             {navItems.map((item, index) => (
               <li key={index}>
@@ -49,6 +53,35 @@ const NavBar = () => {
             ))}
           </ul>
         </nav>
+        {/*mobile nav*/}
+        <div className="block md:hidden">
+          <button className="text-white" onClick={() => setMenuOpen(!menuOpen)}>
+            <Menu className={menuOpen ? "opacity-0" : "opacity-100"} />
+          </button>
+
+          <ul
+            className={` duration-300 space-y-5 w-screen h-screen absolute top-0 right-0 z-30 bg-white bg-opacity-75 p-5 px-10  ${
+              menuOpen ? "translate-x-0" : "translate-x-[200rem] "
+            }`}
+          >
+            <button
+              className="text-primary-dark"
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              <XIcon />
+            </button>
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <Link
+                  href={item.href}
+                  className="flex text-lg items-center gap-1 text-primary-dark hover:text-primary-light duration-150"
+                >
+                  {item.icon} {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </header>
   );
